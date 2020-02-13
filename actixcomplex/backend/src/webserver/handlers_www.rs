3f040b31188c880b62ae2b332b3e39413942f063
Filip Bucek <fbucek@invloop.cz>
2020-02-13 21:04:10 +0100

    handler serving static files from static folder

--- actixcomplex/backend/src/webserver/handlers_www.rs
+++ actixcomplex/backend/src/webserver/handlers_www.rs
@@ -9,6 +9,7 @@ pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.service(index)
         .service(yew)
         .service(index_id_name)
+        .service(actix_files::Files::new("/", "./actixcomplex/backend/static/"))
         .service(password)
         .service(web::scope("/public").service(public_test))
         .service(
