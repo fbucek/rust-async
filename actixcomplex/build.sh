800fbcf7a7e89cef409fbc72d0bd76149382e2ca
Filip Bucek <fbucek@invloop.cz>
2020-02-14 12:53:54 +0100

    actix with compression + wasm optimalization

--- actixcomplex/build.sh
+++ actixcomplex/build.sh
@@ -2,6 +2,11 @@
 
 CURRENT=$(pwd)
 
+# Shrinking wasm size
+# binaryen 
+# `brew install binaryen`
+# @see https://rustwasm.github.io/docs/book/reference/code-size.html#optimizing-builds-for-code-size
+
 # client build
 cd frontend
 
@@ -10,10 +15,14 @@ cargo web deploy --target=wasm32-unknown-unknown --release
 # server build
 cd $CURRENT
 
-
 mkdir -p backend/static
-cp ${CARGO_TARGET_DIR}/deploy/frontend.* $CURRENT/backend/static/
 
+cp ${CARGO_TARGET_DIR}/deploy/frontend.* $CURRENT/backend/static/
+wasm-opt -Oz -o $CURRENT/backend/static/frontend.wasm ${CARGO_TARGET_DIR}/deploy/frontend.wasm 
+
+# Gziping 
+# gzip $CURRENT/backend/static/frontend.wasm
+# mv $CURRENT/backend/static/frontend.wasm.gz $CURRENT/backend/static/frontend.wasm
 
 cd $CURRENT/..
 cargo run --bin actixcomplex
