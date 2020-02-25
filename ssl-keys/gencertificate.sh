35d1f1ee09ca2f574c6082067e560d7a8a754e69
Filip Bucek <fbucek@invloop.cz>
2020-02-25 09:02:22 +0100

    updated certificate generation: added config file

--- ssl-keys/gencertificate.sh
+++ ssl-keys/gencertificate.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function info () {
+    echo -e "[\033[0;34m $@ \033[0m]"
+}
 
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SRCDIR
 
 NAME=rustasync
 
-CONFIG="[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth"
+info "Creating certificates"
+openssl req -new -subj '/CN=rustasync' -config rustasync.config -x509 -out $NAME.crt -keyout $NAME.key
 
-echo "Creating certificates"
-openssl req -x509 -out $NAME.crt -keyout $NAME.key \
-    -newkey rsa:2048 -nodes -sha256 \
-    -subj '/CN=rustasync' -extensions EXT -config <( \
-    printf "[dn]\nCN=rustasync\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-
-echo "Deleting certificat $NAME from system keychain"
+info "Deleting certificat $NAME from system keychain"
 sudo security delete-certificate -c $NAME
 
-echo "Installing $NAME.crt to system keychain"
+info "Installing $NAME.crt to system keychain"
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $NAME.crt
