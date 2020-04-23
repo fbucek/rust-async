53fbad04615ba7162d98ec3c9de3fe5d6a511bc8
Filip Bucek <fbucek@invloop.cz>
2020-04-23 18:34:45 +0200

    certificate stored in keychain only on macOS
    removed email address from config

--- ssl-keys/gencertificate.sh
+++ ssl-keys/gencertificate.sh
@@ -15,15 +15,18 @@ NAME=rustasync
 #   -subj '/CN=rustasync' -extensions EXT -config <( \
 #   printf "[dn]\nCN=rustasync\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
-info "Deleting certificat $NAME from system keychain"
-sudo security delete-certificate -c $NAME
-sudo security delete-certificate -c localhost
-sudo security remove-trusted-cert -d $NAME
+
+if [ $(uname -s) == Darwin ]; then 
+    info "Deleting certificat $NAME from system keychain"
+    sudo security delete-certificate -c $NAME
+fi
 
 if [ ! -z $1 ] && [[ "$1" = "generate" ]];then 
     info "Creating certificates"
     openssl req -new -extensions EXT -config rustasync.config -x509 -out $NAME.crt -keyout $NAME.key
 fi
 
-info "Installing $NAME.crt to system keychain"
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $NAME.crt
+if [ $(uname -s) == Darwin ]; then 
+    info "Installing $NAME.crt to system keychain"
+    sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $NAME.crt
+fi
