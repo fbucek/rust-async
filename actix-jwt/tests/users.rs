68d9c0afbcc7e273b0391c0a84e57b1ae74a81ac
Filip Bucek <fbucek@invloop.cz>
2020-05-07 15:36:49 +0200

    call_service building -> simpler tests

--- actix-jwt/tests/users.rs
+++ actix-jwt/tests/users.rs
@@ -6,16 +6,78 @@ extern crate diesel;
 #[macro_use]
 extern crate diesel_migrations;
 
+use actix_web::{test, web, App, Error};
+use actix_web::dev::{ServiceResponse};
+
+// use actix_http::http::header::{ContentType, Header, HeaderName, IntoHeaderValue};
+// use actix_http::http::{Error as HttpError, Method, StatusCode, Uri, Version};
+// use actix_http::test::TestRequest as HttpTestRequest;
+// use actix_http::{cookie::Cookie, ws, Extensions, HttpService, Request};
+// use actix_router::{Path, ResourceDef, Url};
+use actix_rt::{time::delay_for, System};
+use actix_service::{
+    map_config, IntoService, IntoServiceFactory, Service, ServiceFactory,
+};
+
+
 // This macro from `diesel_migrations` defines an `embedded_migrations` module
 // containing a function named `run`. This allows the example to be run and
 // tested without any outside setup of the database.
 embed_migrations!("migrations");
 //
 
+struct Resp {
+    status: bool,
+    body: Option<String>,
+}
+
+pub async fn call_service_orig<S, R, B, E>(app: &mut S, req: R) -> S::Response
+where
+    S: Service<Request = R, Response = ServiceResponse<B>, Error = E>,
+    E: std::fmt::Debug,
+{
+    app.call(req).await.unwrap()
+}
+
+pub async fn call_service<S, B, E>(app: &mut S) -> S::Response
+where
+    S: Service<Request = actix_http::Request, Response = ServiceResponse<B>, Error = E>,
+    E: std::fmt::Debug,
+{
+    let req = test::TestRequest::get().uri("/users/id").to_request();
+    app.call(req).await.unwrap()
+}
+
+
+// pub async fn get<S, B, E, U>(app: &mut S, url: U) -> Resp
+// where
+// S: Service<Response = ServiceResponse<B>, Error = E>,
+// E: std::fmt::Debug,
+// U: Into<String>,
+// {
+//      // /users/id must pass
+//      let req = test::TestRequest::get().uri("/users/id").to_request();
+//      let resp = test::call_service(&mut app, req).await;
+
+//      assert!(resp.status().is_success());
+//      let body = match resp.response().body().as_ref() {
+//          Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+//          _ => panic!("Response error"),
+//      };
+
+
+//     // Response {
+//     //     status,
+//     //     body,
+//     // }
+// }
+
+
+
+
 mod integrations {
     //#[cfg_attr(test, macro_use)]
     use super::*;
-    use actix_web::{test, web, App, Error};
     use actixjwt::api;
 
     #[actix_rt::test]
@@ -35,10 +97,16 @@ mod integrations {
         )
         .await;
 
+        
+        // FAIL: /fake/users/id
+        let req = test::TestRequest::get().uri("/fake/users/id").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(!resp.status().is_success());
+
+        // /users/id must pass
         let req = test::TestRequest::get().uri("/users/id").to_request();
         let resp = test::call_service(&mut app, req).await;
         assert!(resp.status().is_success());
-
         let response_body = match resp.response().body().as_ref() {
             Some(actix_web::body::Body::Bytes(bytes)) => bytes,
             _ => panic!("Response error"),
