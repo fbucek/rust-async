37d86564ad76e7ba3f172e78bc8bb764ff88e678
Filip Bucek <fbucek@invloop.cz>
2020-05-11 00:31:44 +0200

    using crate testax

--- actix-jwt/src/main.rs
+++ actix-jwt/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{App, HttpServer};
 use diesel::prelude::*;
 
 use actixjwt::{api, db};
--- actix-jwt/tests/users.rs
+++ actix-jwt/tests/users.rs
@@ -6,46 +6,14 @@ extern crate diesel;
 #[macro_use]
 extern crate diesel_migrations;
 
-use actix_web::{test, web, App};
-use actix_web::dev::ServiceResponse;
-use actix_service::Service;
+use actix_web::{test, App};
+
 
 // This macro from `diesel_migrations` defines an `embedded_migrations` module
 // containing a function named `run`. This allows the example to be run and
 // tested without any outside setup of the database.
 embed_migrations!("migrations");
 //
-
-struct RespString {
-    status: actix_web::http::StatusCode,
-    body: String,
-}
-
-
-/// Get method to test Actix server
-/// ```ignore
-/// let resp = get(&mut app, "/fake/users/id").await;
-/// assert_eq!(resp.status.as_u16(), 404);
-/// assert_eq!(resp.body, "");
-/// ```
-async fn get<S, B, E>(mut app: &mut S, url: &str) -> RespString
-where
-    B: actix_http::body::MessageBody,
-    S: Service<Request = actix_http::Request, Response = ServiceResponse<B>, Error = E>,
-    E: std::fmt::Debug,
-{
-    let req = test::TestRequest::get().uri(url).to_request();
-    let resp = test::call_service(&mut app, req).await;
-    let status = resp.status().clone();
-    let body = test::read_body(resp).await;
-    let body = String::from_utf8_lossy(&body).to_string();
-
-    RespString {
-        status,
-        body,
-    }
-}
-
 
 mod integrations {
     //#[cfg_attr(test, macro_use)]
@@ -67,16 +35,15 @@ mod integrations {
         )
         .await;
 
-
-        let resp = get(&mut app, "/fake/users/id").await;
+        let resp = testax::get(&mut app, "/fake/users/id").await;
         assert_eq!(resp.status.as_u16(), 404);
         assert_eq!(resp.body, "");
         
-        let resp = get(&mut app, "/users/1").await;
+        let resp = testax::get(&mut app, "/users/1").await;
         assert_eq!(resp.status.as_u16(), 500); // user does not exists
         assert_eq!(resp.body, "");
 
-        let resp = get(&mut app, "/users").await;
+        let resp = testax::get(&mut app, "/users").await;
         assert_eq!(resp.status.as_u16(), 200);
         assert_eq!(resp.body, "[]");
     }
